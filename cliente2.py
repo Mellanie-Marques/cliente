d23420b208ec9e2cc8a0275a33dc9075f16f397c
Mellanie Marques <103280572+Mellanie-Marques@users.noreply.github.com>
2022-06-23 15:17:05 -0300

    Add files via upload

---
+++ cliente2.py
@@ -0,0 +1,33 @@
+import socket
+
+HOST = 'localhost'     # Endereço IP do Servidor
+
+PORT = 5000            # Porta que o Servidor está
+
+# Criando a conexão
+
+tcp = socket.socket()
+
+destino = (HOST, PORT)
+
+tcp.connect(destino)
+
+print('\nDigite suas mensagens')
+
+print('Para sair use CTRL+X\n')
+
+# Recebendo a mensagem do usuário final pelo teclado
+
+mensagem = input()
+
+# Enviando a mensagem para o Servidor TCP através da conexão
+
+while mensagem != '\x18':
+
+   tcp.send(str(mensagem).encode())
+
+   mensagem = input()
+
+# Fechando o Socket
+
+tcp.close()
